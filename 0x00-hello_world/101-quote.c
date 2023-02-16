86dac20c830893d3eea5f5c0ac8c9db2e5eb1239
kintokeanu <brianislevu@gmail.com>
2023-02-16 14:40:59 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 /**
  * main - main block
+ * void - not accepting args
  * Return: 1
  */
 int main(void)
@@ -10,7 +11,7 @@ int main(void)
 	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 
 	write(2,
-	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	      s);
 	return (1);
 }
