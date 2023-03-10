fb48d2efdacc5e8235551568dda0ae6e5d4ff9f0
kintokeanu <brianislevu@gmail.com>
2023-03-10 13:43:41 -0800

    get change with atoi

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,42 +11,39 @@
 
 int main(int argc, char *argv[])
 {
-        int total, count;
-        unsigned int i;
-        int cents[] = {25, 10, 5, 2};
+	int total, count;
+	unsigned int i;
+	int cents[] = {25, 10, 5, 2};
 
-        if (argc != 2)
-        {
-                printf("Error\n");
-                return (1);
-        }
-
-        total = atoi(argv[1]);
-        count = 0;
-
-        if (total > 0)
-        {
-                while (total > 1)
-                {
-                        for (i = 0; i < sizeof(cents)/sizeof(cents[0]); i++)
-                        {
-                                if (total >= cents[i])
-                                {
-                                        count += total / cents[i];
-                                        total = total % cents[i];
-                                }
-                        }
-                }
-                if (total == 1)
-                        count++;
-        }
-        else
-        {
-                printf("Error\n");
-                return (1);
-        }
-
-        printf("%d\n", count);
-        return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	total = atoi(argv[1]);
+	count = 0;
+	if (total > 0)
+	{
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents) / sizeof(cents[0]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", count);
+	return (0);
 }
 
