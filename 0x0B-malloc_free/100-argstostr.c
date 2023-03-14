c52e5b1f1f90d104933e5c0d43fc29e1d2672c75
kintokeanu <brianislevu@gmail.com>
2023-03-14 12:23:31 -0700

    concatenate argumenst passed in new line

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - Concatenates all the arguments of a program.
+ * @ac: The number of arguments.
+ * @av: An array of pointers to the arguments.
+ *
+ * Return: If concatenation fails or if ac == 0 or av == NULL - NULL.
+ *         Otherwise - a pointer to the new string.
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i, j, k, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
+	}
+	str = malloc(sizeof(char) * (len + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+
+	return (str);
+}
+
