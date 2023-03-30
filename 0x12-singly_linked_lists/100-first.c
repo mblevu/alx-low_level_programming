90173df03d953809a31343ade77890c7bb191116
kintokeanu <brianislevu@gmail.com>
2023-03-30 23:06:39 +0300

    print within function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
  * before_main - tells compiler to execute function before the main()
  */
 
-void __attribute__ ((constructor)) before_main()
+void __attribute__ ((constructor)) before_main(void)
 {
 	printf("You're beat! yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
