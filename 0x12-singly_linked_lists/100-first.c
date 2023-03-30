fe463065dc97c5450b8eb1e207e9563d6056b04d
kintokeanu <brianislevu@gmail.com>
2023-03-30 23:04:29 +0300

    print within function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,7 @@
 
 void __attribute__ ((constructor)) before_main()
 {
-	printf("You're beat! yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
