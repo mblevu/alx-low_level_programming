ef21fd0d1ee8be5b4e9ef5b15ee113a9af37065f
kintokeanu <brianislevu@gmail.com>
2023-03-31 14:03:28 +0300

    get expected output

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
 
 void __attribute__ ((constructor)) before_main(void)
 {
-	printf("You're beat! yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
 
