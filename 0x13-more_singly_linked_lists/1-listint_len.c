e552b5dfbc2b6fdb1661dd629041f327b0ec57c7
kintokeanu <brianislevu@gmail.com>
2023-04-04 16:34:45 +0300

    print list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of number of elements
+ * @:pointer to head node of the list
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
