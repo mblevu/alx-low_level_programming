4ded70886179273443ab2d42b40a1c00a8dc5c4e
kintokeanu <brianislevu@gmail.com>
2023-04-04 19:56:17 +0300

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+
+/**
+ * free_listint2 - free list
+ * @head: double pointer to head
+ * Return: return nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *temp;
+
+	if (head == NULL)
+	{
+		return;
+	}
+	current = *head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+}
