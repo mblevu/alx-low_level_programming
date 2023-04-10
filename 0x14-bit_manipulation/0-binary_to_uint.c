dcc3a3f0dbdb4fe8a7f2b502d5c17cd36faffb84
kintokeanu <brianislevu@gmail.com>
2023-04-10 16:23:12 +0300

    binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,28 +9,24 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	int len = strlen(b);
-	int i;
+	int len, i;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
-
-	for (i = 0; i < len; i++)
+	for (len = 0; b[len]; len++)
 	{
-		if (b[i] == '1')
-		{
-			result = (result << 1) | 1;
-		}
-		else if (b[i] == '0')
-		{
-			result <<= 1;
-		}
-		else
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
 	}
+	for (i = 1, result = 0, len--; len >= 0; len--, i *= 2)
+	{
+		if (b[len] == '1')
+			result += i;
+	}
+
 	return (result);
 }
