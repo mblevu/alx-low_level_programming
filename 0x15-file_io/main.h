30a3362a7afd178d0d04f923652420357ace6f68
kintokeanu <brianislevu@gmail.com>
2023-04-14 03:29:40 +0300

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,11 +10,9 @@
 #include <string.h>
 #include <errno.h>
 
-#define BUFFER_SIZE 1024
+#define MAX_BUF_SIZE 1024
+#define STDERR_FILENO 2
 
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
 
 
 
