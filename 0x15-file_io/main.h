039d93aba18fb29d66ecc8b3ea8256870d5a3aee
kintokeanu <brianislevu@gmail.com>
2023-04-15 03:00:39 +0300

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,11 +9,18 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
+#include <elf.h>
 
 #define MAX_BUF_SIZE 1024
+#define MAXSIZE 1204
 #define STDERR_FILENO 2
 
 
+void print_magic(unsigned char *e_ident);
+Elf64_Ehdr *read_elf_header(int fd);
+void print_elf_osabi(Elf64_Ehdr *header);
+void print_type(Elf64_Ehdr *header);
+void print_entry(Elf64_Ehdr *header);
 
 
 #endif
