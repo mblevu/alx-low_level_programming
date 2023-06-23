6f5d7e540f3a6436c5bd2bd3543493dcdc6af74f
levu <levu@kali>
2023-06-23 23:16:12 +0300

    create dynamic lib

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Compile the .c files into object files
+gcc -Wall -Werror -fPIC -c *.c
+
+# Create the dynamic library
+gcc -shared -o liball.so *.o
+
+# Clean up the object files
+rm *.o
+
+# Move the library to the desired location (optional)
+# mv liball.so /path/to/destination
+
+# Print success message
+echo "Dynamic library liball.so created successfully"
