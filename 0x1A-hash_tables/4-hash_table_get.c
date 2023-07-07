0894c21ad270e2fc905739986b63144bc0c25238
levu <levu@kali>
2023-07-07 03:18:35 +0300

    retrieving value associated with key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_get - retrieve value associated with key
+ * @ht: hashtable to look at
+ * @key: key to look for
+ * Return: value associated with element or null if not found
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
